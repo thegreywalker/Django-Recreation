232c4bd7956642747ea2b28fb7cb8b975df95789
iamapurba203 <new.to.world2003@gmail.com>
2022-09-11 23:45:40 +0530

    Added Some urls concept particularily Mpping Urls, Dynamic Urls amd Redirect Urls using different methods.

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 min every day!",
+    "march": "Learn Django for at least 20 min!",
+    "april": "April Challenge",
+    "may": "May Challenge",
+    "june": "June Challenge",
+    "july": "time for Some Django",
+    "august": "It's August go for a Holiday",
+    "september": "September Challenge",
+    "october": "October Challenge",
+    "november": "It's November ... Time to go out",
+    "december": "December is here!"
+
+}
 
 # Create your views here.
+
+def month_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    
+    if (month > len(months)):
+        return HttpResponseNotFound("Invalid Month!")
+    forward_month = months[month - 1]
+    return HttpResponseRedirect(f"/challenges/{forward_month}")
+
+
+
+def monthly_challenge(request, month): 
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not Supported!")
