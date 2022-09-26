07768ec2dd9326523543e56491e3511eef6b064c
iamapurba203 <new.to.world2003@gmail.com>
2022-09-27 00:29:18 +0530

    Learned about Filters & Tags in Django, will explore more about the Tags like ynamic Url tag etc.

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,8 +1,6 @@
-import django
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -25,13 +23,10 @@ monthly_challenges = {
 def index(request):
     list_items = ""
     months = list(monthly_challenges.keys())
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("Monthly_Challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
 
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months,
+    }) 
 
 
 def month_challenge_by_number(request, month):
@@ -48,7 +43,10 @@ def month_challenge_by_number(request, month):
 def monthly_challenge(request, month): 
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            'challenge_text': challenge_text,
+            "month_name": month
+        })
         # response_data = render_to_string("challenges/challenge.html")
         # return HttpResponse(response_data)
     except:
