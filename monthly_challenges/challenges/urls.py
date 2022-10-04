1b88bf8fcdb4238ba54fcba94bc720c93f971192
iamapurba203 <new.to.world2003@gmail.com>
2022-10-05 01:22:42 +0530

    Cleaning Time

--- monthly_challenges/challenges/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="home"),
-    path("<int:month>", views.month_challenge_by_number, name="Monthly_Challange_by_Number"),
-    path("<str:month>", views.monthly_challenge, name="Monthly_Challenge"),
-]
