f08aafb98f6e6efa94f8c161e2225cf71bac8b97
iamapurba203 <new.to.world2003@gmail.com>
2022-10-05 01:47:01 +0530

    Initial Setup

---
+++ my_site/blog/urls.py
@@ -0,0 +1,9 @@
+from django import views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('/posts', views.posts, name="Posts"),
+    path('/posts/<slug:slug>', views.post_detail, name="Post_Detail")
+]
